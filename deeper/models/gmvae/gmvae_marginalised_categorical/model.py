4904b0cc0426c22903d8f201ca072cf41708dee9
suvalaki <david.ceglia.diaz@gmail.com>
2019-06-29 13:09:11 +1000

    syntax

--- deeper/models/gmvae/gmvae_marginalised_categorical/model.py
+++ deeper/models/gmvae/gmvae_marginalised_categorical/model.py
@@ -490,8 +490,9 @@ class Gmvae(Model):
 
             with writer.as_default():
                 for gradient, variable in zip(gradients, self.trainable_variables):
-                    tf.summary.histogram("gradients/" + variable.name, tf.nn.l2_normalize(gradient))
-                    tf.summary.histogram("variables/" + variable.name, tf.nn.l2_normalize(variable))
+                    stp = tf.summary.experimental.get_step()
+                    tf.summary.histogram("gradients/" + variable.name, tf.nn.l2_normalize(gradient), step=stp)
+                    tf.summary.histogram("variables/" + variable.name, tf.nn.l2_normalize(variable), step=stp)
                 writer.flush()
 
         with tf.device("/gpu:0"):
