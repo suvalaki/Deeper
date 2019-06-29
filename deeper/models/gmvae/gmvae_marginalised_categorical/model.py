b883add565d580fffcaed25091937c39982a74b3
suvalaki <david.ceglia.diaz@gmail.com>
2019-06-29 14:49:41 +1000

    remove bn

--- deeper/models/gmvae/gmvae_marginalised_categorical/model.py
+++ deeper/models/gmvae/gmvae_marginalised_categorical/model.py
@@ -56,8 +56,8 @@ class Encoder(Model):
         for em, bn in zip(self.embeddings, self.embeddings_bn):
             x = em(x)
             #
-            x = tf.nn.tanh(x)
-            x = bn(x, training=training)
+            x = tf.nn.elu(x)
+            #x = bn(x, training=training)
         x = self.latent(x)
         #x = self.latent_bn(x, training=training)
         #x = tf.nn.tanh(x)
