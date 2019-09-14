0a58e7029f1697305274a6f401807ac3bdac452b
suvalaki <david.ceglia.diaz@gmail.com>
2019-09-14 15:04:50 +1000

    initialise categorical encoder testing

---
+++ deeper/tests/layers/categorical.py
@@ -0,0 +1,17 @@
+import unittest
+import tensorflow as tf
+import numpy as np
+from deeper.layers.categorical import CategoricalEncoder
+
+tf.enable_eager_execution()
+
+
+encoder = CategoricalEncoder(
+    latent_dimension=10, 
+    embedding_dimensions=[15,15],
+    embedding_activation=tf.nn.tanh
+)
+
+
+z = np.array([[1,2,3,4,5, 6,7,8,9,10, 11,12,13,14,15]]).astype(float)
+encoder(z)
