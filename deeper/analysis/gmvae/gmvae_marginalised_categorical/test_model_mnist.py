08dc83c40be425910b3961af477241f56340a563
suvalaki <david.ceglia.diaz@gmail.com>
2019-09-14 11:07:33 +1000

    added additional pretraining

--- deeper/analysis/gmvae/gmvae_marginalised_categorical/test_model_mnist.py
+++ deeper/analysis/gmvae/gmvae_marginalised_categorical/test_model_mnist.py
@@ -1,6 +1,8 @@
 #%%
 from pathlib import Path
 import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
 
 tf.enable_eager_execution()
 
@@ -11,6 +13,10 @@ from deeper.models.gmvae.gmvae_marginalised_categorical.utils import (
     chain_call_dataset,
     purity_score,
 )
+from deeper.models.gmvae.gmvae_marginalised_categorical.train import train
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.preprocessing import OneHotEncoder
 
 print("tensorflow gpu available {}".format(tf.test.is_gpu_available()))
 
@@ -42,10 +48,19 @@ X_test = X_test.reshape(X_test.shape[0], 28 * 28)
 X_train = (X_train > 0.5).astype(float)
 X_test = (X_test > 0.5).astype(float)
 
+
+
 #%% Instantiate the model
 from importlib import reload
 
 model = reload(model)
+
+initial_learning_rate = 1e-3
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate,
+    decay_steps=1000,
+    decay_rate=0.8,
+    staircase=True)
 
 m1 = model.Gmvae(
     components=len(set(y_train)),
@@ -54,11 +69,56 @@ m1 = model.Gmvae(
     latent_dimensions=64,
     kind="binary",
     monte_carlo_samples=1,
-    learning_rate=1e-3,
+    learning_rate=initial_learning_rate,
     gradient_clip=10000
 )
 
-from deeper.models.gmvae.gmvae_marginalised_categorical.train import train
+
+
+#%% Examine SOftmax Distribution
+import pandas as pd
+
+logits, prob = m1.graph_qy_g_x.call(X_test, training=False)
+
+logit_df = pd.DataFrame()
+for col in range(logits.shape[1]):
+    temp_df = pd.DataFrame({'value':logits[:,col]})
+    temp_df['k'] = str(col)
+    logit_df = logit_df.append(temp_df, ignore_index=True)
+import seaborn as sns
+sns.violinplot(data=logit_df, y='value' , x='k')
+
+# Plot the correct prediction densities
+
+
+
+# confusion matrix of the classification
+# for each caategory map the appropriate prediction
+confusion_matrix(y_test, np.argmax(m1.predict(X_test),1))
+
+
+#%% Create a kmeans clustering dist
+
+ohe = OneHotEncoder()
+km = MiniBatchKMeans(10)
+km_y_train_idx = km.fit_predict(X_train)
+km_y_test_idx = km.predict(X_test)
+purity_score(y_train, km_y_train_idx)
+km_y_train = ohe.fit_transform(km_y_train_idx.reshape(-1,1)).todense()
+km_y_test = ohe.transform(km_y_test_idx.reshape(-1,1)).todense()
+
+
+#%% Pretrain the model. Each layer individually.
+for i in tqdm(range(10)):
+    idx = np.random.choice(len(X_train), 100)
+    m1.pretrain_categories_step(X_train[idx], np.array(km_y_train[idx]))
+
+# validate the model now matches the pretrained dist
+confusion_matrix(y_test, km_y_test_idx)
+
+for i in tqdm(range(10)):
+    idx = np.random.choice(len(X_train), 100)
+    m1.pretrain_step(X_train[idx], 10)
 
 #%% Train the model
 # with tf.device('/gpu:0'):
@@ -66,12 +126,13 @@ train(
     m1, 
     X_train, y_train, 
     X_test, y_test, 
-    num=10, 
-    samples=1,
+    num=100, 
+    samples=2,
     epochs=10000, 
     iter_train=1, 
     num_inference=1000, 
-    save='model_w'
+    save='model_w',
+    batch=False
 )
 
 
