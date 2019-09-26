6349edd272063eb9b4a05b5827f842999b575d87
suvalaki <david.ceglia.diaz@gmail.com>
2019-09-27 08:47:06 +1000

    Initial commit for GMVAEGAN Sampling model

---
+++ deeper/analysis/gmvaegan/gmvaegan_pure_sampling/test_model_mnist.py
@@ -0,0 +1,190 @@
+#%%
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+from pathlib import Path
+import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
+
+tf.enable_eager_execution()
+tf.set_random_seed(12343411231)
+
+import numpy as np
+from deeper.models.gmvaegan.gmvaegan_pure_sampling import model
+from deeper.models.gmvaegan.gmvaegan_pure_sampling.utils import (
+    chain_call,
+    chain_call_dataset,
+    purity_score,
+)
+from deeper.models.gmvaegan.gmvaegan_pure_sampling.train import (
+    train, 
+    pretrain_with_clusters
+)
+from deeper.utils.cooling import exponential_multiplicative_cooling
+
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.preprocessing import OneHotEncoder
+
+
+
+print("tensorflow gpu available {}".format(tf.test.is_gpu_available()))
+
+#%% Checlk whether the log directory exists. If it does not create it and empty
+logfolder = Path("./logs/test_model/")
+# if logpathis_dir():
+
+gpus = tf.config.experimental.list_physical_devices("GPU")
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            #tf.config.experimental.gpu.set_per_process_memory_fraction(0.9)
+            tf.config.experimental.set_memory_growth(gpu, True)
+            tf.config.experimental.set_synchronous_execution(True)
+            #tf.config.experimental.set_per_process_memory_fraction( 0.9)
+        logical_gpus = tf.config.experimental.list_logical_devices("GPU")
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+#%% Load MNIST and make it binary encoded
+mnist = tf.keras.datasets.mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train, X_test = X_train / 255.0, X_test / 255.0
+X_train = X_train.reshape(X_train.shape[0], 28 * 28)
+X_test = X_test.reshape(X_test.shape[0], 28 * 28)
+X_train = (X_train > 0.5).astype(float)
+X_test = (X_test > 0.5).astype(float)
+
+y_ohe = OneHotEncoder()
+y_train_ohe = np.array(y_ohe.fit_transform(y_train.reshape(-1,1)).todense())
+y_test_ohe = np.array(y_ohe.transform(y_test.reshape(-1,1)).todense())
+
+
+#%% Instantiate the model
+from importlib import reload
+
+model = reload(model)
+
+
+initial_learning_rate = 1e-5
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate,
+    decay_steps=1000,
+    decay_rate=0.8,
+    staircase=True)
+
+m1 = model.GmvaeGan(
+    descriminator_dimensions=[512,128,32],
+    components=len(set(y_train)),
+    input_dimension=X_train.shape[1],
+    embedding_dimensions=[512, 512],
+    embedding_activations=tf.nn.relu,
+    latent_dimensions=64,
+    kind="binary",
+    learning_rate=initial_learning_rate,
+    gradient_clip=1e5,
+    bn_before=False,
+    bn_after=False,
+    categorical_epsilon=0.001,
+    reconstruction_epsilon=0.001,
+    latent_epsilon= 0.001,
+    z_kl_lambda=1.0,
+    c_kl_lambda=1.0,
+    cat_latent_bias_initializer=None,
+    optimizer = tf.keras.optimizers.SGD(
+        initial_learning_rate, 
+        momentum=0.5
+    )
+)
+
+#m1.load_weights('model_w')
+
+#%% Examine SOftmax Distribution
+import pandas as pd
+
+logits, prob = m1.gmvae.graph_qy_g_x.call(X_test, training=False)
+
+logit_df = pd.DataFrame()
+for col in range(logits.shape[1]):
+    temp_df = pd.DataFrame({'value':prob[:,col]})
+    temp_df['k'] = str(col)
+    logit_df = logit_df.append(temp_df, ignore_index=True)
+import seaborn as sns
+sns.violinplot(data=logit_df, y='value' , x='k')
+
+# Plot the correct prediction densities
+
+
+
+# confusion matrix of the classification
+# for each caategory map the appropriate prediction
+confusion_matrix(y_test, np.argmax(m1.predict(X_test),1))
+
+
+#%% Pretrain the model with some known clusters
+if False:
+    pretrain_with_clusters(
+        m1, 
+        X_train, y_train, 
+        X_test, y_test, 
+        num=10, 
+        samples=10,
+        epochs=2, 
+        iter_train=1, 
+        num_inference=1000, 
+        save='model_w',
+    )
+
+
+#%% Train the model
+# with tf.device('/gpu:0'):
+train(
+    m1, 
+    X_train, y_train, 
+    X_test, y_test, 
+    num=10, 
+    samples=1,
+    epochs=1000, 
+    iter_train=10, 
+    num_inference=1000, 
+    save='model_w',
+    batch=True,
+    temperature_function=lambda x: exponential_multiplicative_cooling(
+        x, 1.0, 0.5, 0.98)
+)
+
+
+#%%
+qy_g_x__logit, qy_g_x__prob = m1.graph_qy_g_x(X_train[[9]])
+qy_g_x__ohe =np.array([
+    m1.graph_qy_g_x_ohe(qy_g_x__prob, .005).numpy()[0]
+    for i in range(1000)])
+
+#%%
+logit_df = pd.DataFrame()
+for col in range(qy_g_x__ohe.shape[1]):
+    temp_df = pd.DataFrame({'value':qy_g_x__ohe[:,col]})
+    temp_df['k'] = str(col)
+    logit_df = logit_df.append(temp_df, ignore_index=True)
+import seaborn as sns
+sns.boxplot(data=logit_df, y='value' , x='k')
+
+
+#%%
+for y in m1.entropy_fn(X_train[0:3]):
+    print(tf.shape(y))
+
+#%%
+
+for y in m1.call(X_train[0:3]):
+    print(tf.shape(y))
+
+#%%
+m1.call(X_train[0:3])
+
+#%%
---
+++ deeper/models/gmvaegan/gmvaegan_pure_sampling/model.py
@@ -0,0 +1,377 @@
+import tensorflow as tf
+from tensorflow.python.eager import context
+import numpy as np
+import datetime
+
+from deeper.models.gmvae.gmvae_pure_sampling.model import Gmvae
+from deeper.probability_layers.gumble_softmax import GumbleSoftmaxLayer
+from deeper.probability_layers.normal import RandomNormalEncoder
+from deeper.layers.binary import SigmoidEncoder
+from deeper.layers.categorical import CategoricalEncoder
+from deeper.utils.scope import Scope
+
+tfk = tf.keras
+
+Model = tfk.Model
+
+class GmvaeGan(Model, Scope):
+    def __init__(
+        self,
+        descriminator_dimensions,
+        components,
+        input_dimension,
+        embedding_dimensions,
+        latent_dimensions,
+        descr_embedding_kernel_initializer=tf.initializers.glorot_uniform(),
+        descr_embedding_bias_initializer=tf.initializers.zeros(),
+        descr_latent_kernel_initialiazer=tf.initializers.glorot_uniform(),
+        descr_latent_bias_initializer=tf.initializers.zeros(),
+
+        embedding_activations=tf.nn.relu,
+        mixture_embedding_activations=None,
+        mixture_embedding_dimensions=None,
+        bn_before=False,
+        bn_after=False,
+        categorical_epsilon=0.0,
+        latent_epsilon=0.0,
+        reconstruction_epsilon=0.0,
+        kind="binary",
+        learning_rate=0.01,
+        gradient_clip=None,
+        var_scope='gmvae',
+
+        cat_embedding_kernel_initializer=tf.initializers.glorot_uniform(),
+        cat_embedding_bias_initializer=tf.initializers.zeros(),
+        cat_latent_kernel_initialiazer=tf.initializers.glorot_uniform(),
+        cat_latent_bias_initializer=None,
+
+        latent_mu_embedding_kernel_initializer=tf.initializers.glorot_uniform(),
+        latent_mu_embedding_bias_initializer=tf.initializers.zeros(),
+        latent_mu_latent_kernel_initialiazer=tf.initializers.glorot_uniform(),
+        latent_mu_latent_bias_initializer=tf.initializers.zeros(),
+
+        latent_var_embedding_kernel_initializer=tf.initializers.glorot_uniform(),
+        latent_var_embedding_bias_initializer=tf.initializers.zeros(),
+        latent_var_latent_kernel_initialiazer=tf.initializers.glorot_uniform(),
+        latent_var_latent_bias_initializer=tf.initializers.constant(1.0),
+
+        posterior_mu_embedding_kernel_initializer=tf.initializers.glorot_uniform(),
+        posterior_mu_embedding_bias_initializer=tf.initializers.zeros(),
+        posterior_mu_latent_kernel_initialiazer=tf.initializers.glorot_uniform(),
+        posterior_mu_latent_bias_initializer=tf.initializers.zeros(),
+
+        posterior_var_embedding_kernel_initializer=tf.initializers.glorot_uniform(),
+        posterior_var_embedding_bias_initializer=tf.initializers.zeros(),
+        posterior_var_latent_kernel_initialiazer=tf.initializers.glorot_uniform(),
+        posterior_var_latent_bias_initializer=tf.initializers.constant(1.0),
+
+        recon_embedding_kernel_initializer=tf.initializers.glorot_uniform(),
+        recon_embedding_bias_initializer=tf.initializers.zeros(),
+        recon_latent_kernel_initialiazer=tf.initializers.glorot_uniform(),
+        recon_latent_bias_initializer=tf.initializers.zeros(),
+
+        z_kl_lambda=1.0,
+        c_kl_lambda=1.0,
+
+        optimizer=tf.keras.optimizers.SGD(0.001)
+    ):
+        self.var_scope = 'gmvaegan'
+        self.cooling_distance = 0
+        self.k = components
+        self.gradient_clip = gradient_clip
+        self.optimizer = optimizer
+        super(GmvaeGan, self).__init__()
+
+        self.gmvae = Gmvae(
+            components,
+            input_dimension,
+            embedding_dimensions,
+            latent_dimensions,
+            embedding_activations,
+            mixture_embedding_activations,
+            mixture_embedding_dimensions,
+            bn_before,
+            bn_after,
+            categorical_epsilon,
+            latent_epsilon,
+            reconstruction_epsilon,
+            kind,
+            learning_rate,
+            gradient_clip,
+            var_scope,
+            cat_embedding_kernel_initializer,
+            cat_embedding_bias_initializer,
+            cat_latent_kernel_initialiazer,
+            cat_latent_bias_initializer,
+            latent_mu_embedding_kernel_initializer,
+            latent_mu_embedding_bias_initializer,
+            latent_mu_latent_kernel_initialiazer,
+            latent_mu_latent_bias_initializer,
+            latent_var_embedding_kernel_initializer,
+            latent_var_embedding_bias_initializer,
+            latent_var_latent_kernel_initialiazer,
+            latent_var_latent_bias_initializer,
+            posterior_mu_embedding_kernel_initializer,
+            posterior_mu_embedding_bias_initializer,
+            posterior_mu_latent_kernel_initialiazer,
+            posterior_mu_latent_bias_initializer,
+            posterior_var_embedding_kernel_initializer,
+            posterior_var_embedding_bias_initializer,
+            posterior_var_latent_kernel_initialiazer,
+            posterior_var_latent_bias_initializer,
+            recon_embedding_kernel_initializer,
+            recon_embedding_bias_initializer,
+            recon_latent_kernel_initialiazer,
+            recon_latent_bias_initializer,
+            z_kl_lambda,
+            c_kl_lambda,
+            optimizer
+        )
+        self.descriminator = SigmoidEncoder(
+            latent_dimension=1, 
+            embedding_dimensions=descriminator_dimensions, 
+            var_scope=self.v_name('graph_descriminator'),
+            bn_before=bn_before,
+            bn_after=bn_after,
+            epsilon=0.0,
+            embedding_kernel_initializer=descr_embedding_kernel_initializer,
+            embedding_bias_initializer=descr_embedding_bias_initializer,
+            latent_kernel_initialiazer=descr_latent_kernel_initialiazer,
+            latent_bias_initializer=descr_latent_bias_initializer,
+        )
+
+
+    def sample_one(self, x, training=False, temperature=1.0):
+
+        y_ = tf.cast(
+            tf.fill(tf.stack([tf.shape(x)[0], self.k]), 0.0),
+            dtype=x.dtype
+        )
+        py = tf.cast(tf.fill(tf.shape(y_), 1 / self.k, name="prob"), x.dtype)
+
+        # Sample from the generator
+        (
+            qy_g_x__prob,
+            qz_g_xy__sample,
+            qz_g_xy__logprob,
+            qz_g_xy__prob,
+            pz_g_y__sample,
+            pz_g_y__logprob,
+            pz_g_y__prob,
+            dkl_z_g_xy,
+            px_g_zy__sample,
+            px_g_zy__logprob,
+            px_g_zy__prob,
+        ) = self.gmvae.sample_one(x, training, temperature)
+
+
+        # gmvae Loss parameters
+        # y_entropy
+        dkl_y = y_entropy = tf.reduce_sum(
+            qy_g_x__prob * (tf.math.log(py) - tf.math.log(qy_g_x__prob)),
+            axis=-1,
+        )
+
+        elbo = px_g_zy__logprob + dkl_z_g_xy + dkl_y
+        gmvae_loss = - elbo
+
+
+        # get the prob from the descriminator for the true distribution
+        (
+            descr_true__sample,
+            descr_true__logprob,
+            descr_true__prob
+        ) = self.descriminator.call(x, training)
+
+        # get the prob from the descriminator for the true distribution
+        (
+            descr__sample,
+            descr__logprob,
+            descr__prob
+        ) = self.descriminator.call(px_g_zy__sample, training)
+
+        # desciminator loss
+        descriminator_entropy = (
+            descr_true__logprob + tf.math.log(1-descr__prob)
+        )
+
+        loss = tf.reduce_mean(gmvae_loss - descriminator_entropy, axis=-1)
+
+        return (
+            py,
+            qy_g_x__prob,
+            qz_g_xy__sample,
+            qz_g_xy__logprob,
+            qz_g_xy__prob,
+            pz_g_y__sample,
+            pz_g_y__logprob,
+            pz_g_y__prob,
+            dkl_z_g_xy,
+            px_g_zy__sample,
+            px_g_zy__logprob,
+            px_g_zy__prob,
+            dkl_y,
+            elbo,
+            gmvae_loss,
+            descr_true__sample,
+            descr_true__logprob,
+            descr_true__prob,
+            descr__sample,
+            descr__logprob,
+            descr__prob,
+            descriminator_entropy,
+            loss
+        )
+
+
+    @tf.function(experimental_relax_shapes=True)
+    def sample(self, samples, x, training=False, temperature=1.0):
+        with tf.device("/gpu:0"):
+            result = [self.sample_one(x, training, temperature) for j in range(samples)]
+            result_pivot = list(zip(*result))
+        return result_pivot
+
+
+    @staticmethod
+    @tf.function
+    def mc_stack_mean(x):
+        return tf.reduce_sum(tf.stack(x, 0), 0) / len(x)
+
+
+    @tf.function(experimental_relax_shapes=True)
+    def monte_carlo_estimate(self, samples, x, training=False, temperature=1.0):
+        return [
+            self.mc_stack_mean(z)
+            for z in self.sample(samples, x, training=False, temperature=temperature)
+        ]
+
+
+    @tf.function 
+    def call(self, x, training=False, samples=1, temperature=1.0):
+         return self.monte_carlo_estimate( samples, x, training, temperature)
+
+
+    @tf.function
+    def loss_fn(self, inputs, training=False, samples=1, temperature=1.0):
+        (
+            py,
+            qy_g_x__prob,
+            qz_g_xy__sample,
+            qz_g_xy__logprob,
+            qz_g_xy__prob,
+            pz_g_y__sample,
+            pz_g_y__logprob,
+            pz_g_y__prob,
+            dkl_z_g_xy,
+            px_g_zy__sample,
+            px_g_zy__logprob,
+            px_g_zy__prob,
+            dkl_y,
+            elbo,
+            gmvae_loss,
+            descr_true__sample,
+            descr_true__logprob,
+            descr_true__prob,
+            descr__sample,
+            descr__logprob,
+            descr__prob,
+            descriminator_entropy,
+            loss
+        ) = self.call(inputs, training, samples, temperature)
+
+        return loss
+    
+    def entropy_fn(self, inputs, training=False, samples=1, temperature=1.0):
+        (
+            py,
+            qy_g_x__prob,
+            qz_g_xy__sample,
+            qz_g_xy__logprob,
+            qz_g_xy__prob,
+            pz_g_y__sample,
+            pz_g_y__logprob,
+            pz_g_y__prob,
+            dkl_z_g_xy,
+            px_g_zy__sample,
+            px_g_zy__logprob,
+            px_g_zy__prob,
+            dkl_y,
+            elbo,
+            gmvae_loss,
+            descr_true__sample,
+            descr_true__logprob,
+            descr_true__prob,
+            descr__sample,
+            descr__logprob,
+            descr__prob,
+            descriminator_entropy,
+            loss
+        ) = self.call(inputs, training, samples, temperature)
+
+        return px_g_zy__logprob, dkl_z_g_xy, dkl_y, descriminator_entropy
+
+    @tf.function
+    def train_step(
+        self, 
+        x, 
+        samples=1, 
+        tenorboard=False, 
+        batch=False, 
+        temperature=1.0
+    ):
+
+        if tenorboard:
+            current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            train_log_dir = 'logs/gradient_tape/train'
+
+            writer = tf.summary.create_file_writer(train_log_dir)
+
+
+        # for x in dataset:
+        # Tensorflow dataset is iterable in eager mode
+        with tf.device("/gpu:0"):
+            with tf.GradientTape() as tape:
+                loss = tf.reduce_mean(self.loss_fn(x, True, samples, temperature))
+            # Update ops for batch normalization
+            # update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+            # with tf.control_dependencies(update_ops):
+
+        with tf.device("/gpu:0"):
+            gradients = tape.gradient(loss, self.trainable_variables)
+
+            # Clipping
+            gradients = [
+                None
+                if gradient is None
+                else tf.clip_by_value(
+                    gradient, -self.gradient_clip, self.gradient_clip
+                )
+                #else tf.clip_by_norm(
+                #    gradient, self.gradient_clip
+                #)
+                for gradient in gradients
+            ]
+
+            if tenorboard:
+                with writer.as_default():
+                    for gradient, variable in zip(gradients, self.trainable_variables):
+                        steps = steps + 1
+                        tf.summary.experimental.set_step(steps)
+                        stp = tf.summary.experimental.get_step()
+                        tf.summary.histogram("gradients/" + variable.name, tf.nn.l2_normalize(gradient), step=stp)
+                        tf.summary.histogram("variables/" + variable.name, tf.nn.l2_normalize(variable), step=stp)
+                    writer.flush()
+
+        with tf.device("/gpu:0"):
+            self.optimizer.apply_gradients(
+                zip(gradients, self.trainable_variables)
+            )
+
+    @tf.function
+    def predict(self, x, training=False):
+        qy_g_x__logit, qy_g_x__prob = self.gmvae.graph_qy_g_x(x, training)
+        return qy_g_x__prob
+
+    def increment_cooling(self):
+        self.cooling_distance += 1
+
