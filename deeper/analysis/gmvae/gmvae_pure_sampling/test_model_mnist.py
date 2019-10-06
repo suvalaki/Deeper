d2042f080b67640cc5564c24b4986f63b3b00b71
suvalaki <david.ceglia.diaz@gmail.com>
2019-10-06 23:42:48 +1100

    added additional params

--- deeper/analysis/gmvae/gmvae_pure_sampling/test_model_mnist.py
+++ deeper/analysis/gmvae/gmvae_pure_sampling/test_model_mnist.py
@@ -8,7 +8,8 @@ import tensorflow as tf
 import numpy as np
 from tqdm import tqdm
 
-tf.enable_eager_execution()
+#tf.enable_eager_execution()
+tf.enable_v2_behavior()
 tf.set_random_seed(12343411231)
 
 import numpy as np
@@ -20,6 +21,7 @@ from deeper.models.gmvae.gmvae_pure_sampling.utils import (
 )
 from deeper.models.gmvae.gmvae_pure_sampling.train import (
     train, 
+    train_even,
     pretrain_with_clusters
 )
 from deeper.utils.cooling import exponential_multiplicative_cooling
@@ -71,7 +73,7 @@ from importlib import reload
 model = reload(model)
 
 
-initial_learning_rate = 1e-3
+initial_learning_rate = 3e-4
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
     initial_learning_rate,
     decay_steps=1000,
@@ -81,24 +83,28 @@ lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
 m1 = model.Gmvae(
     components=len(set(y_train)),
     input_dimension=X_train.shape[1],
-    embedding_dimensions=[512, 512],
-    embedding_activations=tf.nn.relu,
+    embedding_dimensions=[512, 256, 128,],
     latent_dimensions=64,
+    mixture_embedding_dimensions = [512, 256, 128, 64,],
+    mixture_latent_dimensions=32,
+    embedding_activations=tf.nn.tanh,
     kind="binary",
     learning_rate=initial_learning_rate,
-    gradient_clip=1e0,
-    bn_before=False,
+    gradient_clip=1e10,
+    bn_before=True,
     bn_after=False,
-    categorical_epsilon=0.0,
-    reconstruction_epsilon=0.0,
-    latent_epsilon= 0.0,
+    categorical_epsilon=1e-12,
+    reconstruction_epsilon=1e-12,
+    latent_epsilon=1e-12,
+    latent_prior_epsilon=1e-0,
     z_kl_lambda=1.0,
     c_kl_lambda=1.0,
     cat_latent_bias_initializer=None,
-    optimizer = tf.keras.optimizers.SGD(
-        initial_learning_rate, 
-        momentum=0.99
-    )
+    optimizer=tf.keras.optimizers.Adam(initial_learning_rate)
+    #optimizer = tf.keras.optimizers.SGD(
+    #    initial_learning_rate, 
+    #    #momentum=0.1
+    #)
 )
 
 #m1.load_weights('model_w')
@@ -138,6 +144,33 @@ pretrain_with_clusters(
     save='model_w',
 )
 
+#%%
+import logging, os
+
+logging.disable(logging.WARNING)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+# Pretraining with even mixture losses
+train_even(
+    m1, 
+    X_train, y_train, 
+    X_test, y_test, 
+    num=100, 
+    samples=1,
+    epochs=10, 
+    iter_train=1, 
+    num_inference=1000, 
+    save='model_w',
+    batch=True,
+    temperature_function=lambda x: exponential_multiplicative_cooling(
+        x, 1., 0.5, 0.98),
+    #temperature_function = lambda x: 0.1
+    save_results='./gumble_results.txt'
+)
+
+
+
+
 
 #%% Train the model
 # with tf.device('/gpu:0'):
@@ -148,12 +181,14 @@ train(
     num=100, 
     samples=1,
     epochs=1000, 
-    iter_train=10, 
+    iter_train=1, 
     num_inference=1000, 
     save='model_w',
     batch=True,
     temperature_function=lambda x: exponential_multiplicative_cooling(
-        x, 1.0, 0.5, 0.98)
+        x, 1., 0.5, 0.98),
+    #temperature_function = lambda x: 0.1
+    save_results='./gumble_results.txt'
 )
 
 
@@ -184,5 +219,86 @@ for y in m1.call(X_train[0:3]):
 
 #%%
 m1.call(X_train[0:3])
+
+#%%
+import matplotlib.pyplot as plt
+import random
+
+images_to_plot = 100
+
+# Get the index for 10 from each predicted category
+cats = np.argmax(m1.predict(X_test),1)
+cats_true = {}
+idx = [i for i in range(len(cats))]
+sample_idx = []
+for col in set(cats):
+  for i in range(10):
+    sample_idx.append(np.random.choice(np.array(idx)[cats==col],1))
+    
+  cats_true.update({
+    str(col): np.argmax(np.bincount(np.array(y_test)[cats==col]))
+  })
+    
+    
+# Get the true labels associated with the majority prediction
+
+
+
+#random_indices = random.sample(range(1000), images_to_plot)
+
+random_indices = sample_idx
+sample_images = X_test[random_indices]
+sample_labels = cats[random_indices]
+plt.clf()
+plt.style.use('seaborn-muted')
+
+fig, axes = plt.subplots(10,10, 
+                         figsize=(15,15),
+                         sharex=True, sharey=True,
+                         subplot_kw=dict(adjustable='box', aspect='equal')) #https://stackoverflow.com/q/44703433/1870832
+
+for i in range(images_to_plot):
+    
+    # axes (subplot) objects are stored in 2d array, accessed with axes[row,col]
+    subplot_row = i//10 
+    subplot_col = i%10
+    ax = axes[subplot_row, subplot_col]
+
+    # plot image on subplot
+    plottable_image = np.reshape(sample_images[i], (28,28))
+    ax.imshow(plottable_image, cmap='gray_r')
+    
+    ax.set_title('Pred Label: {}'.format(cats_true[str(sample_labels[i][0])]))
+    #ax.set_title('Predict Label: {}'.format(cats[i]))
+    ax.set_xbound([0,28])
+
+plt.tight_layout()
+plt.show()
+
+#%%
+# Plot the latent space
+
+latent_vectors = chain_call(m1.call, X_test, 1000)[5]
+
+
+#%%
+# verify sklearn gaussian mixture?
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from matplotlib import pyplot as plt
+pca = PCA(2)
+X_pca = pca.fit_transform(latent_vectors)
+kmeans = KMeans(10)
+pred = kmeans.fit_predict(X_pca)
+print(purity_score(y_test, pred))
+
+df_latent = pd.DataFrame({
+    'x1':X_pca[:,0], 
+    'x2':X_pca[:,1], 
+    'cat':['pred_{}'.format(i) for i in y_test]
+})
+plt.figure(figsize=(10,10))
+sns.scatterplot(data=df_latent,x='x1',y='x2',hue='cat')
+
 
 #%%
