fa8f3e58508e830c67fb1401598654dd3d021f7c
suvalaki <david.ceglia.diaz@gmail.com>
2019-10-12 15:11:55 +1100

    syntax

--- deeper/ops/distance.py
+++ deeper/ops/distance.py
@@ -6,8 +6,7 @@ def kl_divergence(px, qx, axis=-1, name='kl_divergence'):
     dkl(P|Q) = integrate_(x) P * (log(P) - log(Q))
     """
     dkl = tf.reduce_sum(
-        tf.math.multiply(px, tf.math.subtract(tf.log(px), tf.log(qx)
-        ), 
+        tf.math.multiply(px, tf.math.subtract(tf.log(px), tf.log(qx))), 
         axis=axis, 
         name=name
     )
