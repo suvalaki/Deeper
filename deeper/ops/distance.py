9f77742a38a4404da708d95b36e9699b6d6d604f
suvalaki <david.ceglia.diaz@gmail.com>
2019-10-12 15:11:19 +1100

    updates

--- deeper/ops/distance.py
+++ deeper/ops/distance.py
@@ -2,6 +2,14 @@ import tensorflow as tf
 import numpy as np 
 
 def kl_divergence(px, qx, axis=-1, name='kl_divergence'):
-    dkl = tf.reduce_sum(px * (tf.log(px) - tf.log(qx)), axis=axis, name=name)
+    """
+    dkl(P|Q) = integrate_(x) P * (log(P) - log(Q))
+    """
+    dkl = tf.reduce_sum(
+        tf.math.multiply(px, tf.math.subtract(tf.log(px), tf.log(qx)
+        ), 
+        axis=axis, 
+        name=name
+    )
     return dkl
 
