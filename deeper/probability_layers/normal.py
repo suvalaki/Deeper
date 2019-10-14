f0e1da662cf2e0cdb99a84bb376da2593a3f2b85
suvalaki <david.ceglia.diaz@gmail.com>
2019-10-14 17:20:14 +1100

    update normal embedding to not have duble dimension

--- deeper/probability_layers/normal.py
+++ deeper/probability_layers/normal.py
@@ -16,17 +16,17 @@ class RandomNormalEncoder(Layer, Scope):
         var_scope='normal_encoder',
         bn_before=False,
         bn_after=False,
-        epsilon=1e-8,
+        epsilon=0.0,
 
-        embedding_mu_kernel_initializer=tf.initializers.glorot_uniform(),
+        embedding_mu_kernel_initializer=tf.initializers.glorot_normal(),
         embedding_mu_bias_initializer=tf.initializers.zeros(),
         latent_mu_kernel_initialiazer=tf.initializers.glorot_normal(),
         latent_mu_bias_initializer=tf.initializers.zeros(),
 
-        embedding_var_kernel_initializer=tf.initializers.glorot_uniform(),
+        embedding_var_kernel_initializer=tf.initializers.glorot_normal(),
         embedding_var_bias_initializer=tf.initializers.zeros(),
-        latent_var_kernel_initialiazer=tf.initializers.glorot_uniform(),
-        latent_var_bias_initializer=tf.initializers.ones()
+        latent_var_kernel_initialiazer=tf.initializers.glorot_normal(),
+        latent_var_bias_initializer=tf.initializers.ones(),
 
         fixed_mu=None,
         fixed_var=None,
@@ -94,7 +94,7 @@ class RandomNormalEncoder(Layer, Scope):
             self.mu_logvar = Encoder(
                 latent_dim=2*self.latent_dimension,
                 embedding_dimensions=[
-                    2*x for x in self.embedding_dimensions
+                    x for x in self.embedding_dimensions
                 ],
                 activation=self.embedding_activation,
                 var_scope=self.v_name('mu_encoder'), 
@@ -123,42 +123,62 @@ class RandomNormalEncoder(Layer, Scope):
             mu = self.mu(inputs, training)
             logvar = self.logvar(inputs, training)
         else:
-            mu_logvar = self.mu_logvar(inputs, training)
+            mu_logvar = self.mu_logvar(inputs, training) 
             mu, logvar = tf.split(mu_logvar, 2, axis=-1)
 
         if apply_epsilon:
-            var = tf.exp(logvar) + self.epsilon
+            var = tf.add(tf.exp(logvar), self.epsilon)
             logvar = tf.math.log(var)
 
         mu = tf.identity(mu, name=self.v_name('mu'))
         logvar = tf.identity(logvar, name=self.v_name('logvar'))
+        var = tf.exp(logvar, name=self.v_name('var'))
 
-        return mu, logvar
+        return mu, logvar, var
+
 
     @tf.function
     def call_mu(self, inputs, training=False):
-        mu, logvar = self.call_parameters(inputs, training)
+        """Get the mean of the network given the inputs"""
+        mu, logvar, var = self.call_parameters(inputs, training)
         return mu
+
 
     @tf.function
     def call_logvar(self, inputs, training=False, apply_epsilon=True):
-        mu, logvar = self.call_parameters(inputs, training, apply_epsilon)
+        """Get the logvar of the network given the inputs"""
+        mu, logvar, var = self.call_parameters(inputs, training, apply_epsilon)
         return logvar
 
-    @tf.function 
-    def _sample_fn(self, mu=0.0, var=1.0, name='sample'):
 
-        # reparmeterisation trick
+    @tf.function
+    def call_var(self, inputs, training=False, apply_epsilon=True):
+        """Get the var of the network given the inputs"""
+        mu, logvar, var = self.call_parameters(inputs, training, apply_epsilon)
+        var = tf.exp(logvar)
+        return var
+
+
+    @staticmethod
+    @tf.function 
+    def _sample_fn( mu, var):
+        """Reparameterisation trick sample from a random normal distribution
+        with the given mean (mu) and variance (var)
+
+        Paramters:
+        ----------
+        mu: tf.tensor mean of the distribution
+        var: tf.tensor, variance of the distribution
+        name: name of output sample tensor
+        """
+        sample_shape = tf.shape(mu)
         r_norm = tf.cast(
             tf.random.normal( sample_shape, mean=0., stddev=1.),
-            inputs.dtype
+            mu.dtype
         )
-        sample = tf.identity(
-            mu + r_norm * tf.math.sqrt(var)
-            ,name=self.v_name(name)
-        )
-
+        sample = tf.add(mu, tf.multiply(r_norm, tf.sqrt(var)))
         return sample
+
 
     @tf.function
     def sample(
@@ -168,29 +188,37 @@ class RandomNormalEncoder(Layer, Scope):
         apply_epsilon=False,
         name='sample'
     ):
+        """Sample from this distribution layer. 
 
-        mu, logvar = self.call_parameters(inputs, training, apply_epsilon)
-        sample = self._sample_fn(mu, logvar, name)
+        Parameters:
+        -----------
+        inputs: Input vector to the 
+        training: bool flag for whether the layer is training
+        apply_epsilon: bool flag for whether toa djust the logvar by epsilon
+        name: name of output sample tensor
+        """
+
+        mu, logvar, var = self.call_parameters(inputs, training, apply_epsilon)
+        sample = self._sample_fn(mu, var)
         return sample
 
 
     @tf.function
     def call(self, inputs, training=False, outputs=None, apply_epsilon=True):
 
-        mu, logvar = self.call_parameters(inputs, training)
-        var = tf.exp(logvar)
+        mu, logvar, var = self.call_parameters(inputs, training, apply_epsilon)
 
         if outputs is not None:
             # Sample was given. We calc probs given these values
             sample = outputs
         else:
-            sample = self._sample_fn(mu, logvar)
+            sample = self._sample_fn(mu, var)
         
         # Metrics for loss
-        logprob = self.logprob(sample, mu, var),
+        logprob = self.logprob(sample, mu, var)
         prob = tf.exp(logprob)
+        return sample, logprob, prob, mu, logvar, var
 
-        return sample, logprob, prob, mu, logvar
 
     @staticmethod
     @tf.function
@@ -199,37 +227,21 @@ class RandomNormalEncoder(Layer, Scope):
         if eps > 0.0:
             var = tf.add(var, eps, name='clipped_var')
 
-        logprob = -0.5 * tf.reduce_sum(
-                tf.math.log(2 * tf.cast(np.pi, x.dtype)) 
-                + tf.math.log(var) + tf.square(x - mu) / var
-            , axis
+        kernel = - 0.5 * (
+            tf.math.log(2 * tf.cast(np.pi, mu.dtype)) 
+            + tf.math.log(var) + tf.square(x - mu) / var
         )
-        #logprob = tf.clip_by_value(logprob, np.log(1e-32), np.log(1-1e-32))
+
+        logprob = tf.reduce_sum(kernel, axis)
         return logprob
 
     @tf.function
     def logprob(self, x, mu, var, axis=-1):
-        return self._log_normal(x, mu, var, self.epsilon, axis)
+        return self._log_normal(x, mu, var, 0.0, axis)
 
     @tf.function 
     def prob(self, x, mu, logvar, axis=-1):
         return tf.exp(self.logprob(x, mu, var, axis))
-
-    @tf.function
-    def entropy(self, inputs, inputs_x, mu_x, var_x, training=False, 
-        eps=0.0, eps_x=0.0, axis=-1):
-        """Compare mu and var against """
-        mu = self.mu(inputs, training)
-        logvar = self.logvar(inputs, training)
-        #mu_logvar = self.mu_logvar(inputs, training)
-        #mu, logvar = tf.split(mu_logvar, 2, axis=-1)
-        var = tf.math.exp(logvar)  
-
-        entropy = (self.log_normal(inputs_x, mu, var, eps) 
-            - self.log_normal(inputs_x, mu_x, var_x, eps_x))
-
-        return entropy
-
 
 
 def lognormal_pdf(x, mu, logvar, eps=0.0, axis=-1, clip=1e-18):
@@ -250,12 +262,11 @@ def lognormal_pdf(x, mu, logvar, eps=0.0, axis=-1, clip=1e-18):
 def lognormal_kl(x, mu_x, mu_y, logvar_x, logvar_y, 
     eps_x=0.0, eps_y=0.0, axis=-1, clip=1e-24):
 
-    """
-    var_x = tf.math.log(logvar_x)
+    var_x = tf.math.exp(logvar_x)
     if eps_x > 0.0:
         var_x = tf.add(var_x, eps_x)
 
-    var_y = tf.math.log(logvar_y)
+    var_y = tf.math.exp(logvar_y)
     if eps_y > 0.0:
         var_y = tf.add(var_y, eps_y)
 
@@ -264,11 +275,6 @@ def lognormal_kl(x, mu_x, mu_y, logvar_x, logvar_y,
         + var_x / var_y 
         + tf.square(mu_x - mu_y) / var_y - 1
         , axis=-1
-    )
-    """
-    entropy = tf.math.subtract(
-        lognormal_pdf(x, mu_y, logvar_y, eps_y)
-        ,lognormal_pdf(x, mu_x, logvar_x, eps_x)
     )
 
     return entropy
