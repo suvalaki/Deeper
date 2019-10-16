1404be65b74f2e6a6c48e633278888b19f951ba8
suvalaki <david.ceglia.diaz@gmail.com>
2019-10-16 22:33:37 +1100

    change epoch counter to cooling distance param

--- deeper/models/gmvae/gmvae_pure_sampling/train.py
+++ deeper/models/gmvae/gmvae_pure_sampling/train.py
@@ -116,7 +116,7 @@ def train(
                 "{:10.5f}\t{:10.5f}\t"
                 "{:10.5f}\t{:10.5f}\t"
                 "{:10.2f}\t{:10.5f}".format(
-                    i,
+                    int(model.cooling_distance),
                     loss,
                     recon,
                     z_ent,
@@ -258,6 +258,146 @@ def pretrain_with_clusters(
             )
             if save is not None:
                 model.save_weights(save, save_format='tf')
+
+        #t1.update(1)
+        #t2.n = 0
+        #t2.last_print_n = 0
+        #t2.refresh()
+    #t1.close()
+
+
+def train_even(
+    model, 
+    X_train, 
+    y_train, 
+    X_test, 
+    y_test, 
+    num, 
+    samples,
+    epochs, 
+    iter_train, 
+    num_inference, 
+    batch=False,
+    verbose=1, 
+    save=None,
+    temperature_function=None,
+    save_results=None
+):
+
+    #t1 = tqdm(total=epochs, position=0)
+    #t2 = tqdm(total=int(X_train.shape[0] // num), position=1, leave=False)
+
+    if save_results is not None:
+
+        header_str = ((
+            "{:<10}\t{:<10}\t{:<10}\t{:<10}\t"
+            "{:<10}\t{:<10}\t{:<10}\t{:<10}\t"
+            "{:<10}\t{:<10}\t{:<10}"
+            ).format(
+                "epoch",
+                "loss",
+                "likelih",
+                "z-prior",
+                "y-prior",
+                "trAMI",
+                "teAMI",
+                "trPUR",
+                "tePUR",
+                'attch_te',
+                "temp"
+            )
+        )
+
+        save_results = os.path.join(os.path.abspath(save_results))
+
+        if not os.path.exists(save_results):
+            with open(save_results, 'w') as results_file:
+                results_file.write(header_str)   
+
+
+    tqdm.write(header_str)
+
+
+    for i in range(epochs):
+
+        
+
+        # Setup datasets
+        dataset_train = (
+            tf.data.Dataset.from_tensor_slices(X_train)
+            .repeat(iter_train)
+            .shuffle(X_train.shape[0])
+            .batch(num)
+        )
+
+
+        for x in dataset_train:
+            model.pretrain_step(x,samples=samples, batch=batch)
+        
+        #for i in range(iter):
+        #    idx=np.random.choice(len(X_train), num)
+        #    model.train_step(X_train[idx])
+        #    t2.update(1)
+        #t2.close()
+
+        if i % verbose == 0:
+            # Evaluate training metrics
+            recon, z_ent, y_ent = chain_call(model.entropy_fn, X_train, num_inference)
+
+            recon = np.array(recon).mean()
+            z_ent = np.array(z_ent).mean()
+            y_ent = np.array(y_ent).mean()
+
+            loss = -(recon + z_ent + y_ent)
+
+            idx_tr = chain_call(model.predict, X_train, num_inference).argmax(1)
+            idx_te = chain_call(model.predict, X_test, num_inference).argmax(1)
+
+            ami_tr = adjusted_mutual_info_score(
+                y_train, idx_tr, average_method="arithmetic"
+            )
+            ami_te = adjusted_mutual_info_score(
+                y_test, idx_te, average_method="arithmetic"
+            )
+
+            attch_te = (
+                np.array(np.unique(idx_te, return_counts=True)[1]).max()
+                / len(idx_te)
+            )
+
+            purity_train = purity_score(y_train, idx_tr)
+            purity_test = purity_score(y_test, idx_te)
+
+            value_str = (
+                "{:d}\t{:10.5f}\t{:10.5f}\t{:10.5f}\t{:10.5f}\t"
+                "{:10.5f}\t{:10.5f}\t"
+                "{:10.5f}\t{:10.5f}\t"
+                "{:10.2f}\t{:10.5f}".format(
+                    i,
+                    loss,
+                    recon,
+                    z_ent,
+                    y_ent,
+                    ami_tr,
+                    ami_te,
+                    purity_train,
+                    purity_test,
+                    attch_te,
+                    0.0
+                )
+            )
+
+            if save_results is not None:
+                with open(save_results, 'a') as results_file:
+                    results_file.write('\n'+value_str)   
+
+
+
+            tqdm.write(value_str)
+            
+            if save is not None:
+                model.save_weights(save, save_format='tf')
+
 
         #t1.update(1)
         #t2.n = 0
