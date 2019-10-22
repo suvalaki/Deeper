f937f3d4a771548b3c6bbafbbdeb74d104aa582b
suvalaki <david.ceglia.diaz@gmail.com>
2019-10-22 13:24:41 +1100

    added model pred to plot

--- deeper/models/gmvae/gmvae_marginalised_categorical/train.py
+++ deeper/models/gmvae/gmvae_marginalised_categorical/train.py
@@ -16,7 +16,7 @@ import pandas as pd
 import io
 
 
-def plot_latent(latent_vectors, y_test):
+def plot_latent(latent_vectors, y_test, y_pred):
     pca = PCA(2)
     #pca = TSNE(2)
     X_pca = pca.fit_transform(latent_vectors)
@@ -27,10 +27,12 @@ def plot_latent(latent_vectors, y_test):
         'x1':X_pca[:,0], 
         'x2':X_pca[:,1], 
         'cat':y_test,#['pred_{}'.format(i) for i in y_test],
-        'kmeans':pred#['pred_{}'.format(i) for i in pred]
+        'kmeans':y_pred#['pred_{}'.format(i) for i in pred]
     })
 
     #fig, ax = plt.subplots()
+
+    #km_pur = purity_score()
 
     f, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(20,10))
 
@@ -38,12 +40,14 @@ def plot_latent(latent_vectors, y_test):
     ax1.scatter(
         df_latent.x1, df_latent.x2, c=df_latent.cat, cmap='viridis'
     )
+    ax1.set_title('True Labels. VAE purity ()')
 
     #fig2, ax2 = plt.subplots()
     #pred_scatter = sns.scatterplot(data=df_latent,x='x1',y='x2',hue='kmeans', ax=ax2)
     ax2.scatter(
-        df_latent.x1, df_latent.x2, c=df_latent.cat, cmap='viridis'
+        df_latent.x1, df_latent.x2, c=df_latent.kmeans, cmap='viridis'
     )
+    ax2.set_title('Latent Clustering Labels. Purity (pred)')
 
     return f
     #return true_scatter, pred_scatter
@@ -175,7 +179,8 @@ def train(
             )
             plt_latent_true  = plot_latent(
                 latent_vectors, 
-                y_test
+                y_test,
+                idx_te
             )
 
             with summary_writer.as_default():
