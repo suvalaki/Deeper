ee5d408f7ef8f66c3bd6e25c9aedafdb7c0ac09b
suvalaki <david.ceglia.diaz@gmail.com>
2019-11-10 01:04:32 +1100

    added purity score metrics

---
+++ deeper/utils/metrics.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn import metrics
+
+def purity_score(y_true, y_pred):
+    # compute contingency matrix (also called confusion matrix)
+    contingency_matrix = metrics.cluster.contingency_matrix(y_true, y_pred)
+    # return purity
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(
+        contingency_matrix
+    )
