1624895702d98011b1f00580d5f013dfb4970abb
David Diaz <david.ceglia.diaz@gmail.com>
2019-12-18 15:11:01 +1100

    Puresample: feature. add train from config
    cooling.py: Added extra hooks for static Cooling methods
    wrapper.py: Added config parser to parse training configs from json

--- deeper/utils/cooling.py
+++ deeper/utils/cooling.py
@@ -43,6 +43,25 @@ def quadratic_multiplicative_cooling(k, start, end, alpha):
     return shifted_base * cooling + end
 
 
+class CallableTempCls:
+    def __init__(self, method, start, end, alpha):
+        """Object to hold the cooling method for this
+
+        method: name of the cooling regime to implement
+
+        """
+        self.method = method
+        self.start = start
+        self.end = end
+        self.alpha = alpha
+        self.iter = 0
+
+    def __call__(self, x):
+        self.iter = x
+        value = self.method(self.iter, self.start, self.end, self.alpha)
+        return value
+
+
 class ConstantRegime:
     def __init__(self, const=1.0):
         self.const = const
@@ -101,7 +120,7 @@ class CyclicCoolingRegime:
         return value
 
 
-def get_regime(identifier):
+def get_method(identifier):
     """A dict of the form {class_name, class_args...}
 
     """
@@ -117,10 +136,11 @@ def get_regime(identifier):
     return mappings[identifier]
 
 
-def get(identifier: dict):
+def get_regime(identifier: dict):
     """A dict of the form {class_name, class_args}"""
-    class_args = {k: v for k, v in identifier.items() if k is not "class_name"}
+    class_args = {k: v for k, v in identifier.items() if k != "class_name"}
     mappings = {
+        "static": CallableTempCls,
         "constant": ConstantRegime,
         "infinite": InfiniteCoolingRegime,
         "cyclic": CyclicCoolingRegime,
@@ -128,7 +148,7 @@ def get(identifier: dict):
 
     if "method" in class_args:
         # Get the appropriate method
-        class_args["method"] = get_regime(class_args["method"])
+        class_args["method"] = get_method(class_args["method"])
 
     regime = mappings[identifier["class_name"]](**class_args)
     return regime
