4cbf73bf6a0020dccacbf1c9ac2d7efb434c2037
david <david.ceglia.diaz@gmail.com>
2021-06-28 19:04:37 +1000

    Added tile ops for scalars

---
+++ deeper/ops/dims.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+
+def tile_scalar_value(x, shape):
+    """Replicates the scalar value over a tensor of given shape"""
+    return tf.ones(shape) * x
+
+
+def tile_scalar_value_for_concat(x, y):
+    """Expand the dimensions on a scalar value so that it can be
+    concat on the last dimension of another tensor"""
+    return tile_scalar_value(x, (*tf.shape(y)[:-1], 1))
