3a69d93b44d1c07b3f72d453bdcfd17d2541b61c
david <david.ceglia.diaz@gmail.com>
2021-12-17 22:58:22 +1100

    Add an encoder baseclass

--- deeper/utils/feature_engineering/encoder.py
+++ deeper/utils/feature_engineering/encoder.py
@@ -10,3 +10,23 @@ def ohe_to_ordinalohe(data, axis=-1, threshold=0.5):
         data,
     )
     return dat
+
+
+def index_to_ohe(index: int, max_index: int = 0) -> np.ndarray:
+    if max_index == 0:
+        return np.array([])
+    x = np.zeros(max_index)
+    x[index] = 1
+    return x
+
+
+class IndexOhe:
+    def __init__(self, max_index: int):
+        self.max_index = max_index
+
+    def fit(self):
+        # does nothing
+        return self
+
+    def transform(self, x):
+        return index_to_ohe(x, self.max_index)
