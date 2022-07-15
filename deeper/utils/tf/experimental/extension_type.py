da3f4c7fade9b9fde91fca4926f1dca8a0e18d2a
david <david.ceglia.diaz@gmail.com>
2022-07-15 13:46:38 +1000

    Added a unpacker method for extension types

--- deeper/utils/tf/experimental/extension_type.py
+++ deeper/utils/tf/experimental/extension_type.py
@@ -19,3 +19,29 @@ class ExtensionTypeIterableMixin:
 
     def __len__(self):
         return len(self._tf_extension_type_fields())
+
+    @classmethod
+    def extract_unpacked(cls, x, i, n, axis=0):
+        if type(x) == tf.Tensor:
+            return tf.unstack(x, n, axis=0)[i]
+        return type(x)(
+            **{
+                k: cls.extract_unpacked(v, i, n, axis)
+                if isinstance(v, tf.experimental.ExtensionType)
+                else tuple(cls.extract_unpacked(z, i, n, axis) for z in v)
+                if type(v) == tuple
+                else cls.extract_unpacked(v, i, n, axis)
+                for k, v in x._asdict().items()
+            }
+        )
+
+
+def factory_from_named_tuple(x):
+    field_types = x.__annotations__
+
+    class NewClass(tf.experimental.ExtensionType):
+        __name__ = x.__name__ + "ExtensionType"
+        for z, k in field_types.items():
+            locals()[z] = k
+
+    return Name
